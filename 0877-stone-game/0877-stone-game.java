beebc8902c76f1c4467e3167b571920e71423f1c
Bhavik Sahu <bhaviksahu03@gmail.com>
2026-08-02 10:50:50 +0530

    Time: 10 ms (10.85%), Space: 46.9 MB (10.85%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,24 @@
+class Solution {
+    int[][] dp;
+    public boolean stoneGame(int[] nums) {
+        int n = nums.length;
+        dp = new int[n][n];
+
+        for(int[] row:dp){
+            Arrays.fill(row,Integer.MIN_VALUE);
+
+        }
+        return solve(nums,0,n-1)>=0;
+    }
+    private int solve(int[] nums,int i,int j){
+        if(i==j)return nums[i];
+
+        if(dp[i][j]!=Integer.MIN_VALUE)return dp[i][j];
+
+        int takeleft = nums[i]-solve(nums,i+1,j);
+        int takeright = nums[j]-solve(nums,i,j-1);
+
+        return dp[i][j]=Math.max(takeleft,takeright);
+        
+    }
+}
