b68bc4807dc0a962d4742b7f8c6df8d3ddafe314
Bhavik Sahu <bhaviksahu03@gmail.com>
2026-09-06 22:04:05 +0530

    Time: 26 ms (9%), Space: 54.2 MB (62.97%) - LeetHub

---
+++ 0054-distinct-subsequences/0054-distinct-subsequences.java
@@ -0,0 +1,28 @@
+class Solution {
+    private int solve(int i, int j, String s,String t, int[][] memo){
+        if(j==t.length())return 1;
+        if(i==s.length())return 0;
+        if(memo[i][j]!=-1){
+            return memo[i][j];
+        }
+        int negtake = solve(i+1,j,s,t,memo);
+        int take = 0;
+
+        if(s.charAt(i)==t.charAt(j)){
+            take = solve(i+1,j+1,s,t,memo);
+        }
+        return memo[i][j]=take+negtake;
+        
+    }
+    public int numDistinct(String s, String t) {
+        int  n = s.length();
+        int m = t.length();
+int[][] memo = new int[n][m];
+for(int[] row:memo){
+Arrays.fill(row,-1);
+
+}
+        return solve(0,0,s,t,memo);
+
+    }
+}
